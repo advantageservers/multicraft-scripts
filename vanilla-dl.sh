8cf5fcef05f958a8d58207bf1e834359e120ed9a
Dylan Hansch <dylan@dylanhansch.net>
2015-05-25 09:16:15 -0500

    Create vanilla-dl.sh

---
+++ vanilla-dl.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Version 1.8.6
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.6/minecraft_server.1.8.6.jar -O vanilla-1.8.6.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.8.6.jar.conf --no-check-certificate
+
+# Version 1.8.5
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.5/minecraft_server.1.8.5.jar -O vanilla-1.8.5.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.8.5.jar.conf --no-check-certificate
+
+# Version 1.8.4
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.4/minecraft_server.1.8.4.jar -O vanilla-1.8.4.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.8.4.jar.conf --no-check-certificate
+
+# Version 1.8.3
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.3/minecraft_server.1.8.3.jar -O vanilla-1.8.3.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.8.3.jar.conf --no-check-certificate
+
+# Version 1.8.2
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.2/minecraft_server.1.8.2.jar -O vanilla-1.8.2.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.8.2.jar.conf --no-check-certificate
+
+# Version 1.8.1
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.1/minecraft_server.1.8.1.jar -O vanilla-1.8.1.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.8.1.jar.conf --no-check-certificate
+
+# Version 1.8
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar -O vanilla-1.8.0.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.8.0.jar.conf --no-check-certificate
+
+# Version 1.7.10
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.7.10/minecraft_server.1.7.10.jar -O vanilla-1.7.10.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.7.10.jar.conf --no-check-certificate
+
+# Version 1.7.9
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.7.9/minecraft_server.1.7.9.jar -O vanilla-1.7.9.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.7.9.jar.conf --no-check-certificate
+
+# Version 1.7.5
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.7.5/minecraft_server.1.7.5.jar -O vanilla-1.7.5.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.7.5.jar.conf --no-check-certificate
+
+# Version 1.7.4
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar -O vanilla-1.7.4.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.7.4.jar.conf --no-check-certificate
+
+# Version 1.7.2
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.7.2/minecraft_server.1.7.2.jar -O vanilla-1.7.2.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/vanilla-1.7.2.jar.conf --no-check-certificate
