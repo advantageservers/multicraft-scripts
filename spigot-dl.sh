cffed2c021839c928764dbc7ac7d5a1092cb4cd2
Dylan Hansch <dylan@dylanhansch.net>
2015-08-02 22:28:23 -0500

    Closes #1

---
+++ spigot-dl.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Version 1.8.8
+wget http://repo.advantageservers.com/jars/spigot-1.8.8.jar -O spigot-1.8.8.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.8.8.jar.conf --no-check-certificate
+
+# Version 1.8.7
+wget http://repo.advantageservers.com/jars/spigot-1.8.7.jar -O spigot-1.8.7.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.8.7.jar.conf --no-check-certificate
+
+# Version 1.8.6
+wget http://repo.advantageservers.com/jars/spigot-1.8.6.jar -O spigot-1.8.6.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.8.6.jar.conf --no-check-certificate
+
+# Version 1.8.5
+wget http://repo.advantageservers.com/jars/spigot-1.8.5.jar -O spigot-1.8.5.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.8.5.jar.conf --no-check-certificate
+
+# Version 1.8.4
+wget http://repo.advantageservers.com/jars/spigot-1.8.4.jar -O spigot-1.8.4.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.8.4.jar.conf --no-check-certificate
+
+# Version 1.8.3
+wget http://repo.advantageservers.com/jars/spigot-1.8.3.jar -O spigot-1.8.3.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.8.3.jar.conf --no-check-certificate
+
+# Version 1.8.0
+wget http://repo.advantageservers.com/jars/spigot-1.8.0.jar -O spigot-1.8.0.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.8.0.jar.conf --no-check-certificate
+
+# Version 1.7.10
+wget http://repo.advantageservers.com/jars/spigot-1.7.10.jar -O spigot-1.7.10.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.7.10.jar.conf --no-check-certificate
+
+# Version 1.7.9
+wget http://repo.advantageservers.com/jars/spigot-1.7.9.jar -O spigot-1.7.9.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.7.9.jar.conf --no-check-certificate
+
+# Version 1.7.5
+wget http://repo.advantageservers.com/jars/spigot-1.7.5.jar -O spigot-1.7.5.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.7.5.jar.conf --no-check-certificate
+
+# Version 1.7.2
+wget http://repo.advantageservers.com/jars/spigot-1.7.2.jar -O spigot-1.7.2.jar --no-check-certificate
+wget http://repo.advantageservers.com/conf/spigot-1.7.2.jar.conf --no-check-certificate
