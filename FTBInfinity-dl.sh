13f914ea0c532e885f53227821ac47a799747efe
Dylan Hansch <dylan@dylanhansch.net>
2015-08-19 13:57:11 -0500

    FTBInfinity to 1.10.1

--- FTBInfinity-dl.sh
+++ FTBInfinity-dl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## declare an array variable
-versions=("1.8.2" "1.7.0" "1.6.2")
+versions=("1.6.2" "1.7.0" "1.8.2" "1.10.1")
 
 ## now loop through the above array
 for version in "${versions[@]}"
