2a1beca5e5ae0b9158f5373fc9936df5c48b60a7
Dylan Hansch <dylan@dylanhansch.net>
2015-08-19 13:57:02 -0500

    FTBDirewolf20 to 1.7.0

--- FTBDirewolf20-dl.sh
+++ FTBDirewolf20-dl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## declare an array variable
-versions=("1.4.1" "1.5.0" "1.6.1")
+versions=("1.4.1" "1.5.0" "1.6.1" "1.7.0")
 
 ## now loop through the above array
 for version in "${versions[@]}"
